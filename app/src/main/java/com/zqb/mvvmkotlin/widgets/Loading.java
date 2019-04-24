6daa787828454db7c6093c5f1d669a3c77149ed4
zqb <zhangqianbin3@foxmail.com>
2019-04-24 17:43:19 +0800

    .

---
+++ app/src/main/java/com/zqb/mvvmkotlin/widgets/Loading.java
@@ -0,0 +1,29 @@
+package com.zqb.mvvmkotlin.widgets;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.zqb.mvvmkotlin.R;
+
+public class Loading extends Dialog {
+    public Loading(@NonNull Context context) {
+        this(context, R.style.BaseDialog);
+    }
+
+    public Loading(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected Loading(@NonNull Context context, boolean cancelable,  @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_load);
+        setCanceledOnTouchOutside(false);
+    }
+}
